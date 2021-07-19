eec11fbe3573236253fe5e53e360224ad115b149
Afonso Neto <afonsoneto121@gmail.com>
2021-07-19 16:47:52 -0300

    home

---
+++ src/theme.js
@@ -0,0 +1,12 @@
+const theme = {
+  colors: {
+    primary: '#6200ee',
+    background: '#00000014',
+    text: '#000000D3',
+    success: '#135E01'
+},
+  fonts:{
+    regular: 'Roboto, sans-serif'
+  }
+}
+export default theme
